d63413431dcf1ee189c2c48c6ded3a56ebb0d264
Max Herman <mherman25@Maxs-MacBook-Pro-2.local>
2025-01-29 09:03:45 -0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * HelloWorld.java
+ * My first Java program!!!
+ * @author Max Herman
+ * @version 2024-08-28
+ */
+
+ public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        // This is the only working line of code in this program!
+        System.out.println("Hello, World!");
+
+    }
+}
