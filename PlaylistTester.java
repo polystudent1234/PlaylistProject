73289ae1847e6e5f15d1ae5ecb04ee4499dbbdfa
Connor Poon <cpoon25@students.polytechnic.org>
2025-02-03 13:55:36 -0800

    did part of the tester

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -7,31 +7,50 @@
  * @version
  */
 public class PlaylistTester {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        int passed = 0;
         System.out.println("Initializing a Playlist...\n");
-        //Make your playlist here
-
+        
+        Playlist playlist = new Playlist();
+        passed ++;
+        System.out.println("Tests passed: " + "(" + passed + "/10)");
+        
         System.out.println("Adding songs to the Playlist...\n");
         /**
          * Add some songs here. Note that the format for adding a Song to a Playlist p
          * is something like...
          * p.addSong(new Song(..., ..., ...))
          */
-
-
-
+        playlist.addSong(new Song("Symphony No. 2 in C minor (Auferstehung): V. Im Tempo des Scherzos. Wild herausfahrend", "Gustav Mahler, Leonard Bernstein, London Symphony Orchestra, Edinburgh Festival Chorus, Janet Baker, Shelia Armstrong", 2183));
+        playlist.addSong(new Song("N95", "Kendrick Lamar", 227));
+        playlist.addSong(new Song("Dark Red", "Steve Lacy", 174));
+        playlist.addSong(new Song("Sibelius: Violin Concerto in D Minor, Op. 47: I. Allegro moderato", "Jean Sibelius, Itzhak Perlman, Andre Previn, Pittsburgh Symphony Orchestra", 971));
+        playlist.addSong(new Song("The Rite of Spring, K15, Pt. 1: VII. Adoration of the Earth (The Sage)", "Igor Stravinsnky, Berliner Philharmoniker, Herbert von Karajan", 25));
+        passed ++;
+        System.out.println("Tests passed: " + "(" + passed + "/10)");
+        
         System.out.println("Printing the songs...\n");
-        //Print out all the songs in the playlist to verify it's working correctly
-
-
+        playlist.examineAll();
+        passed ++;
+        System.out.println("Tests passed: " + "(" + passed + "/10)");
+        
         System.out.println("\nLiking the songs in position X, Y, Z, etc....\n");
-        //Once your songs are 'liked', this should be reflected in the next printout
-
+        playlist.like(playlist.callSong(0), true);
+        playlist.like(playlist.callSong(2), true);
+        playlist.like(playlist.callSong(4), true);
+        passed ++;
+        System.out.println("Tests passed: " + "(" + passed + "/10)");
 
         System.out.println("Printing the songs...\n");
-
+        playlist.examineAll(); 
+        passed ++;
+        System.out.println("Tests passed: " + "(" + passed + "/10)");
 
         System.out.println("\nRemoving the song in position A, B, C, etc...\n");
+        playlist.removeSong(playlist.callSong(1));
+        passed ++;
+        System.out.println("Tests passed: " + "(" + passed + "/10)");
 
 
         System.out.println("Printing the songs...\n");
